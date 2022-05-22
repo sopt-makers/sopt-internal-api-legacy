386b2122652de13ef8456cd8b164e9a8d259cd05
eunsukimme <eunsu.dev@gmail.com>
2022-05-22 16:52:42 +0900

    feat: add logger and built-in middlewares

--- src/infrastructure/webserver/server.ts
+++ src/infrastructure/webserver/server.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import logger from "morgan";
+import path from "path";
 
 import { createServices } from "@/application/service";
 import { bootstrap } from "@/infrastructure/config/bootstrap";
@@ -7,6 +9,11 @@ import { createRoutes } from "@/interfaces/routes";
 
 export async function createServer() {
   const app = express();
+
+  app.use(logger("dev"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, "public")));
 
   const db = await bootstrap();
 
