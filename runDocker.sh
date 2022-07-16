eb402c05ef23c79af0e46fb1f32dd17c59b10daa
eunsukimme <eunsu.dev@gmail.com>
2022-07-16 19:31:33 +0900

    build: edit docker shell file to stop and remove existing container and remove danglign images

--- runDocker.sh
+++ runDocker.sh
@@ -1,3 +1,8 @@
 docker build . -t sopt-core-api-server
 
+docker stop sopt-core-api-server
+docker rm sopt-core-api-server
+
 docker run -d -p 4000:4000 --name sopt-core-api-server sopt-core-api-server
+
+docker image prune -f
